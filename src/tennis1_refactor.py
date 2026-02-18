42bf4d91fbe5e0c1d60735fcbd408d3f90aee0ee
juan071825 <juanmateoalvarez@gmail.com>
2026-02-18 17:06:52 +0100

    feat(tennis1_refactor.py): :sparkles: Añadir módulo de refactorización.

---
+++ src/tennis1_refactor.py
@@ -0,0 +1,61 @@
+class TennisGame1:
+
+    # Inicializador de clase.
+    def __init__(self, player1_name, player2_name):
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.p1points = 0
+        self.p2points = 0
+
+
+
+    # Función que suma un punto a la puntuación del ganador del punto.
+    def won_point(self, player_name):
+        if player_name == "player1":
+            self.p1points += 1
+        else:
+            self.p2points += 1
+
+
+
+    # Función que determina el marcador de un juego en un determinado momento.
+    def score(self):
+        result = ""
+        temp_score = 0
+        
+        # Caso 1: si las puntuaciones son iguales.
+        if self.p1points == self.p2points:
+            result = {
+                0: "Love-All",
+                1: "Fifteen-All",
+                2: "Thirty-All",
+            }.get(self.p1points, "Deuce") # Para cualquier valor mayor que 3 devuelve "Deuce".
+        
+        
+        # Caso 2: victoria o ventaja.
+        elif self.p1points >= 4 or self.p2points >= 4:
+            minus_result = self.p1points - self.p2points
+            if minus_result == 1:
+                result = "Advantage player1"
+            elif minus_result == -1:
+                result = "Advantage player2"
+            elif minus_result >= 2:
+                result = "Win for player1"
+            else:
+                result = "Win for player2"
+        
+        # Caso 3: marcador dónde uno va ganando pero no tiene ventaja.
+        else:
+            for i in range(1, 3):
+                if i == 1:
+                    temp_score = self.p1points
+                else:
+                    result += "-"
+                    temp_score = self.p2points
+                result += {
+                    0: "Love",
+                    1: "Fifteen",
+                    2: "Thirty",
+                    3: "Forty",
+                }[temp_score]
+        return result
