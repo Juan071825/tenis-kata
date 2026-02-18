e094754be673449f5d721ccdadd647251f100103
juan071825 <juanmateoalvarez@gmail.com>
2026-02-18 17:05:06 +0100

    test(tennis_test.py): :test_tube: Añadir caso test para tennis1.

---
+++ test/tennis_test.py
@@ -0,0 +1,20 @@
+import pytest
+from src.tennis1_refactor import TennisGame1
+from .tennis_unittest import play_game, test_cases
+
+
+@pytest.mark.parametrize(
+    "TennisGameClass",
+    [
+        TennisGame1,
+
+    ],
+)
+@pytest.mark.parametrize(
+    "p1_points p2_points score p1_name p2_name".split(), test_cases
+)
+def test_get_score_most_games(
+    TennisGameClass, p1_points, p2_points, score, p1_name, p2_name
+):
+    game = play_game(TennisGameClass, p1_points, p2_points, p1_name, p2_name)
+    assert score == game.score()
